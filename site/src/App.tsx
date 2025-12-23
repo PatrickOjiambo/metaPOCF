eabaa9f1bf8ddfdecc6021c720a2b7d0f3258f64
Patrick Ojiambo <pashrick237@gmail.com>
2025-12-23 15:46:13 +0300

    feat: shadcn setup

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Button } from './components/ui/button'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,12 +12,13 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <Button>Button</Button>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="text-red-700">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
