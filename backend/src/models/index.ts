2b5a96a6307a2ba9163927be52843c74115c6629
Patrick Ojiambo <pashrick237@gmail.com>
2026-01-02 13:27:56 +0300

    feat: Moved backend folder

--- backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { Treasury, type ITreasury, type ITransaction, type IReward } from "./treasury.model.js";
-export { ProcessedEvent, type IProcessedEvent } from "./processed-event.model.js";
-export { Snapshot, type ISnapshot, type ISnapshotEntry } from "./snapshot.model.js";
-export { AdminNonce, type IAdminNonce } from "./admin-nonce.model.js";
-export { DrawHistory, type IDrawHistory, type IWinner } from "./draw-history.model.js";
